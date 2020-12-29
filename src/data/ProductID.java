91f2a2aeebe28a85e5932f2cf42e10715b06abab
llobera00 <64154175+llobera00@users.noreply.github.com>
2020-12-29 13:35:33 +0100

    Update ProductID.java

--- src/data/ProductID.java
+++ src/data/ProductID.java
@@ -1,4 +1,27 @@
 package data;
 
-public class ProductID {
+final public class ProductID {
+  private final string productID
+  
+  public ProductID(string code){
+    this.productID = code;
+  }
+  
+  public String getProductID{
+    return productID;
+  }
+  
+  public boolean equals (object o){
+    if(this==o) return true;
+    if(o==null ||  getClass != o.getClass) return false
+    productID prodID = (productID)o
+    return productID.equals (prodID.productID);
+  }
+  
+  public int hashCode(){
+    return productID.hashCode();
+  }
+  public String toString(){
+  return "ProductID{" + "personal code='" + productID + '\'' + '}';
+  }
 }
